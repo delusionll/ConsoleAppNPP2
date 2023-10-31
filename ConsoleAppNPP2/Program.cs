b34fb1a98eb8d4b1d9b36afbaa3887254a198f61
Lev Zernov <lev.zernov@outlook.com>
2023-11-01 00:10:07 +0300

    init

--- ConsoleAppNPP2/Program.cs
+++ ConsoleAppNPP2/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System.Security.Cryptography;
 
-namespace ConsoleAppNPP2
+class Program
 	{
-	internal class Program
+	static void Main(string[] args)
 		{
-		static void Main(string[] args)
+		Console.WriteLine("arg0 - <FilePath> (C:/...), arg1 - <SegmentSize> in bytes");
+
+		// User Inputs
+		string inputFilePath = Console.ReadLine();
+		int inputSegmentSize = int.Parse(Console.ReadLine());
+
+		SegmentHasher(inputFilePath, inputSegmentSize);
+
+		// SegmentHasherMethod
+		static void SegmentHasher(string filePath, int segmentSize)
 			{
-			Console.WriteLine("Hello, World!");
+			using(FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+				{
+				using(SHA256 sha256 = SHA256.Create())
+					{
+					byte[] buffer = new byte[segmentSize];
+					int bytesRead;
+
+					while((bytesRead=fileStream.Read(buffer, 0, buffer.Length))>0)
+						{
+						byte[] segmentHash = sha256.ComputeHash(buffer, 0, bytesRead);
+						string segmentHashHex = BitConverter.ToString(segmentHash).Replace("-", "").ToLower();
+						Console.WriteLine($"Segment Hash: {segmentHashHex}");
+						}
+					}
+				}
 			}
 		}
 	}
