d2541b2cd21d59dda706d329fc65b166e1a3dcff
Lev Zernov <lev.zernov@outlook.com>
2023-10-31 16:30:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppNPP2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+
+namespace ConsoleAppNPP2
+	{
+	internal class Program
+		{
+		static void Main(string[] args)
+			{
+			Console.WriteLine("Hello, World!");
+			}
+		}
+	}
