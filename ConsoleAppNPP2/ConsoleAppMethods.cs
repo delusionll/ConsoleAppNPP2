d37ac66a3cd1da9f618f1a3fe4dcd0d66ac23ee2
Lev Zernov <lev.zernov@outlook.com>
2023-11-02 02:11:43 +0300

    code review

---
+++ ConsoleAppNPP2/ConsoleAppMethods.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace ConsoleAppNPP2
+	{
+	public static class ConsoleAppMethods
+		{
+		// SegmentHasher Method
+		public static void SegmentHasher(string filePath, int segmentSize)
+			{
+			Stopwatch stopWatch = new Stopwatch();
+			stopWatch.Start();
+
+			using(FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+				{
+				using(SHA256 sha256 = SHA256.Create())
+					{
+					byte[] buffer = new byte[segmentSize];
+					int bytesRead;
+					long fileSize = fileStream.Length;
+
+					Parallel.For(0, (int)(fileSize/segmentSize), i =>
+					{
+						// Monopoly
+						lock(fileStream)
+							{
+							fileStream.Seek(i*segmentSize, SeekOrigin.Begin);
+							bytesRead=fileStream.Read(buffer, 0, buffer.Length);
+							}
+
+						byte[] segmentHash = sha256.ComputeHash(buffer, 0, bytesRead);
+						string segmentHashHex = BitConverter.ToString(segmentHash).ToLower();
+						Console.WriteLine($"Segment Hash: {segmentHashHex}");
+					}
+					);
+					}
+				}
+
+			stopWatch.Stop();
+			Console.WriteLine($"Elapsed time is {stopWatch.ElapsedMilliseconds} ms");
+			}
+		}
+	}
--- ConsoleAppNPP2/Program.cs
+++ ConsoleAppNPP2/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Security.Cryptography;
+﻿using ConsoleAppNPP2;
 
 class Program
 	{
@@ -13,42 +12,7 @@ class Program
 			string inputFilePath = Console.ReadLine();
 			int inputSegmentSize = int.Parse(Console.ReadLine());
 
-			SegmentHasher(inputFilePath, inputSegmentSize);
-
-			// SegmentHasher Method
-			static void SegmentHasher(string filePath, int segmentSize)
-				{
-				Stopwatch stopWatch = new Stopwatch();
-				stopWatch.Start();
-
-				using(FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-					{
-					using(SHA256 sha256 = SHA256.Create())
-						{
-						byte[] buffer = new byte[segmentSize];
-						int bytesRead;
-						long fileSize = fileStream.Length;
-
-						Parallel.For(0, (int)(fileSize/segmentSize), i =>
-							{
-								// Monopoly
-								lock(fileStream)
-									{
-									fileStream.Seek(i*segmentSize, SeekOrigin.Begin);
-									bytesRead=fileStream.Read(buffer, 0, buffer.Length);
-									}
-
-								byte[] segmentHash = sha256.ComputeHash(buffer, 0, bytesRead);
-								string segmentHashHex = BitConverter.ToString(segmentHash).ToLower();
-								Console.WriteLine($"Segment Hash: {segmentHashHex}");
-							}
-						);
-						}
-					}
-
-				stopWatch.Stop();
-				Console.WriteLine($"Elapsed time is {stopWatch.ElapsedMilliseconds}");
-				}
+			ConsoleAppMethods.SegmentHasher(inputFilePath, inputSegmentSize);
 
 			Console.WriteLine("Continue? y/n");
 			string keepGoing = Console.ReadLine();
