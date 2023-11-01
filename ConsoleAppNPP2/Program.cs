1603587d2b513a20706a0a956ec2b393c085c4f4
Lev Zernov <lev.zernov@outlook.com>
2023-11-01 18:15:48 +0300

    continue?

--- ConsoleAppNPP2/Program.cs
+++ ConsoleAppNPP2/Program.cs
@@ -4,40 +4,55 @@ class Program
 	{
 	static void Main(string[] args)
 		{
+
 		Console.WriteLine("arg0 - <FilePath> (C:/...), arg1 - <SegmentSize> in bytes");
 
-		// User Inputs
-		string inputFilePath = Console.ReadLine();
-		int inputSegmentSize = int.Parse(Console.ReadLine());
-
-		SegmentHasher(inputFilePath, inputSegmentSize);
-
-		// SegmentHasher Method
-		static void SegmentHasher(string filePath, int segmentSize)
+		while(true)
 			{
-			using(FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-				{
-				using(SHA256 sha256 = SHA256.Create())
-					{
-					byte[] buffer = new byte[segmentSize];
-					int bytesRead;
-					long fileSize = fileStream.Length;
 
-					Parallel.For(0, (int)(fileSize/segmentSize), i =>
+			// User Inputs
+			string inputFilePath = Console.ReadLine();
+			int inputSegmentSize = int.Parse(Console.ReadLine());
+
+			SegmentHasher(inputFilePath, inputSegmentSize);
+
+			// SegmentHasher Method
+			static void SegmentHasher(string filePath, int segmentSize)
+				{
+				using(FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+					{
+					using(SHA256 sha256 = SHA256.Create())
 						{
-							// Monopoly 
-							lock(fileStream)
-								{
-								fileStream.Seek(i * segmentSize, SeekOrigin.Begin);
-								bytesRead=fileStream.Read(buffer, 0, buffer.Length);
-								}
+						byte[] buffer = new byte[segmentSize];
+						int bytesRead;
+						long fileSize = fileStream.Length;
+
+						Parallel.For(0, (int)(fileSize/segmentSize), i =>
+							{
+								// Monopoly 
+								lock(fileStream)
+									{
+									fileStream.Seek(i*segmentSize, SeekOrigin.Begin);
+									bytesRead=fileStream.Read(buffer, 0, buffer.Length);
+									}
 								byte[] segmentHash = sha256.ComputeHash(buffer, 0, bytesRead);
 								string segmentHashHex = BitConverter.ToString(segmentHash).ToLower();
 								Console.WriteLine($"Segment Hash: {segmentHashHex}");
 
+							}
+						);
 						}
-					);
 					}
+				}
+			Console.WriteLine("Continue? y/n");
+			string keepGoing = Console.ReadLine();
+			if(keepGoing=="y")
+				{
+				continue;
+				}
+			else
+				{
+				break;
 				}
 			}
 		}
